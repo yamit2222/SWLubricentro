eac6c37398859515b169e93111f0d1252d059810
yamit2222 <62761614+yamit2222@users.noreply.github.com>
2026-01-05 05:10:38 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,33 +3,21 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default defineConfig({
   plugins: [react()],
-  preview: {port:1657, host:true},
-  server: {
-    allowedHosts: true,
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://146.83.198.35:80',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
+  preview: {port:1658, host:true},
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@context': path.resolve(__dirname, './src/context'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(dirname, './src/components'),
+      '@hooks': path.resolve(dirname, './src/hooks'),
+      '@context': path.resolve(dirname, './src/context'),
+      '@pages': path.resolve(dirname, './src/pages'),
+      '@services': path.resolve(dirname, './src/services'),
+      '@styles': path.resolve(dirname, './src/styles'),
+      '@assets': path.resolve(dirname, './src/assets'),
       '@helpers': path.resolve(__dirname, './src/helpers')
     }
   }
